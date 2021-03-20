5b9448812afd7ccdfd2c264b82b5d082ff851a8b
Kaue-G <kaueguedes.dev@gmail.com>
2021-03-20 17:56:49 -0300

    :package: Rodapé

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-document.querySelector(".hamburguer").addEventListener("click" , () => 
-        document.querySelector(".container").classList.toggle("show-menu")
-    );
+// document.querySelector(".hamburguer").addEventListener("click" , () => 
+//         document.querySelector(".container").classList.toggle("show-menu")
+//     );
+
+// var hamburguer = document.querySelector(".hamburguer");
+
+// hamburguer.addEventListener("click", function() {
+//     document.querySelector(".sidebar").classList.toggle("show-menu");
+// });
+
+function show (){;
+    document.querySelector(".container").classList.toggle("show-menu")
+};
