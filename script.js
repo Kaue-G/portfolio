4091b5d408ae8e6734e816e84170e45ed3189279
Kaue-G <kaueguedes.dev@gmail.com>
2021-03-24 13:20:47 -0300

    teste

--- script.js
+++ script.js
@@ -11,3 +11,11 @@
 function show (){;
     document.querySelector(".container").classList.toggle("show-menu")
 };
+
+document.querySelector("#qtde").addEventListener("click", atualizarPreco);
+
+function atualizarPreco(){
+const qtde = document.querySelector("#qtde").nodeValue;
+
+console.log(qtde);
+}
